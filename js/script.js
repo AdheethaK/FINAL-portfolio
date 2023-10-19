2c986617c7d04e467fd64bcfb37202153fccdf2d
ANK <kalupahanaadheetha5@gmail.com>
2023-10-20 02:11:41 +0530

    create entire assignment page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$("#assignments>div:first-of-type>h1").on('click',()=>{
+    console.log("assignment 01 heading clicked! :)")
+})
+$("#assignments>div:nth-of-type(2)>h1").on('click',()=>{
+    console.log("assignment 02 heading clicked! :)")
+})
+$("#assignments>div:nth-of-type(3)>h1").on('click',()=>{
+    console.log("assignment 03 heading clicked! :)")
+})
+$("#assignments>div:nth-of-type(4)>h1").on('click',()=>{
+    console.log("assignment 04 heading clicked! :)")
+})
+$("#assignments>div:nth-of-type(5)>h1").on('click',()=>{
+    console.log("assignment 05 heading clicked! :)")
+})
