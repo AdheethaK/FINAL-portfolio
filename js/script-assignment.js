f798001f591cc5d0e1520f2c6fcdc4730c77ad86
ANK <kalupahanaadheetha5@gmail.com>
2023-10-23 03:42:24 +0530

    add radio button navigation with case panes for all assignments

--- js/script-assignment.js
+++ js/script-assignment.js
@@ -52,3 +52,160 @@ $('#assignment-4-radio').click(function () {
         $('.assignment label:nth-of-type(4)').css('-webkit-text-fill-color','#0013E6')
     }
 })
+
+//-----------------case pane navigation for assignment 1-----------------
+//change to assignment 1 case 1 pane
+$('#assignment-1-case-1-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-1-case-1-pane').css('opacity','1')
+        $('#assignment-1-case-2-pane').css('opacity','0')
+        $('#assignment-1-case-3-pane').css('opacity','0')
+        $('#assignment-1-case-4-pane').css('opacity','0')
+        $('#assignment-1-case-5-pane').css('opacity','0')
+        $('#assignment-1-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 1 case 2 pane
+$('#assignment-1-case-2-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-1-case-1-pane').css('opacity','0')
+        $('#assignment-1-case-2-pane').css('opacity','1')
+        $('#assignment-1-case-3-pane').css('opacity','0')
+        $('#assignment-1-case-4-pane').css('opacity','0')
+        $('#assignment-1-case-5-pane').css('opacity','0')
+        $('#assignment-1-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 1 case 3 pane
+$('#assignment-1-case-3-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-1-case-1-pane').css('opacity','0')
+        $('#assignment-1-case-2-pane').css('opacity','0')
+        $('#assignment-1-case-3-pane').css('opacity','1')
+        $('#assignment-1-case-4-pane').css('opacity','0')
+        $('#assignment-1-case-5-pane').css('opacity','0')
+        $('#assignment-1-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 1 case 4 pane
+$('#assignment-1-case-4-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-1-case-1-pane').css('opacity','0')
+        $('#assignment-1-case-2-pane').css('opacity','0')
+        $('#assignment-1-case-3-pane').css('opacity','0')
+        $('#assignment-1-case-4-pane').css('opacity','1')
+        $('#assignment-1-case-5-pane').css('opacity','0')
+        $('#assignment-1-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 1 case 5 pane
+$('#assignment-1-case-5-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-1-case-1-pane').css('opacity','0')
+        $('#assignment-1-case-2-pane').css('opacity','0')
+        $('#assignment-1-case-3-pane').css('opacity','0')
+        $('#assignment-1-case-4-pane').css('opacity','0')
+        $('#assignment-1-case-5-pane').css('opacity','1')
+        $('#assignment-1-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 1 case 6 pane
+$('#assignment-1-case-6-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-1-case-1-pane').css('opacity','0')
+        $('#assignment-1-case-2-pane').css('opacity','0')
+        $('#assignment-1-case-3-pane').css('opacity','0')
+        $('#assignment-1-case-4-pane').css('opacity','0')
+        $('#assignment-1-case-5-pane').css('opacity','0')
+        $('#assignment-1-case-6-pane').css('opacity','1')
+    }
+})
+//-----------------case pane navigation for assignment 2-----------------
+//change to assignment 2 case 1 pane
+$('#assignment-2-case-1-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-2-case-1-pane').css('opacity','1')
+        $('#assignment-2-case-2-pane').css('opacity','0')
+        $('#assignment-2-case-3-pane').css('opacity','0')
+        $('#assignment-2-case-4-pane').css('opacity','0')
+        $('#assignment-2-case-5-pane').css('opacity','0')
+        $('#assignment-2-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 2 case 2 pane
+$('#assignment-2-case-2-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-2-case-1-pane').css('opacity','0')
+        $('#assignment-2-case-2-pane').css('opacity','1')
+        $('#assignment-2-case-3-pane').css('opacity','0')
+        $('#assignment-2-case-4-pane').css('opacity','0')
+        $('#assignment-2-case-5-pane').css('opacity','0')
+        $('#assignment-2-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 2 case 3 pane
+$('#assignment-2-case-3-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-2-case-1-pane').css('opacity','0')
+        $('#assignment-2-case-2-pane').css('opacity','0')
+        $('#assignment-2-case-3-pane').css('opacity','1')
+        $('#assignment-2-case-4-pane').css('opacity','0')
+        $('#assignment-2-case-5-pane').css('opacity','0')
+        $('#assignment-2-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 2 case 4 pane
+$('#assignment-2-case-4-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-2-case-1-pane').css('opacity','0')
+        $('#assignment-2-case-2-pane').css('opacity','0')
+        $('#assignment-2-case-3-pane').css('opacity','0')
+        $('#assignment-2-case-4-pane').css('opacity','1')
+        $('#assignment-2-case-5-pane').css('opacity','0')
+        $('#assignment-2-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 2 case 5 pane
+$('#assignment-2-case-5-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-2-case-1-pane').css('opacity','0')
+        $('#assignment-2-case-2-pane').css('opacity','0')
+        $('#assignment-2-case-3-pane').css('opacity','0')
+        $('#assignment-2-case-4-pane').css('opacity','0')
+        $('#assignment-2-case-5-pane').css('opacity','1')
+        $('#assignment-2-case-6-pane').css('opacity','0')
+    }
+})
+//change to assignment 2 case 6 pane
+$('#assignment-2-case-6-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-2-case-1-pane').css('opacity','0')
+        $('#assignment-2-case-2-pane').css('opacity','0')
+        $('#assignment-2-case-3-pane').css('opacity','0')
+        $('#assignment-2-case-4-pane').css('opacity','0')
+        $('#assignment-2-case-5-pane').css('opacity','0')
+        $('#assignment-2-case-6-pane').css('opacity','1')
+    }
+})
+//-----------------case pane navigation for assignment 3-----------------
+//change to assignment 3 case 1 pane
+$('#assignment-3-case-1-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-3-case-1-pane').css('opacity','1')
+        $('#assignment-3-case-2-pane').css('opacity','0')
+    }
+})
+//change to assignment 3 case 2 pane
+$('#assignment-3-case-2-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-3-case-1-pane').css('opacity','0')
+        $('#assignment-3-case-2-pane').css('opacity','1')
+    }
+})
+//-----------------case pane navigation for assignment 4-----------------
+//change to assignment 4 case 1 pane
+$('#assignment-4-case-1-radio').click(function () {
+    if($(this).is(':checked')){
+        $('#assignment-4-case-1-pane').css('opacity','1')
+    }
+})
